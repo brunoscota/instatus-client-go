69beddb6310d4b8a230ac56cb6866eb50ed6bdf9
Bruno Scota de Carvalho <brunoscota@gmail.com>
2024-05-09 11:41:53 -0300

    iniciar

--- component.go
+++ component.go
@@ -1,31 +1,54 @@
 package instatus
 
+import "time"
+
 const componentName = "component"
 
 type Component struct {
-	Name        *string `json:"name"`
+	ID                         *string            `json:"id"`
+	Name                       *string            `json:"name,omitempty"`
+	NameTranslationID          *string            `json:"nameTranslationId,omitempty"`
+	Description                *string            `json:"description,omitempty"`
+	DescriptionTranslationID   *string            `json:"descriptionTranslationId,omitempty"`
+	Status                     *string            `json:"status,omitempty"`
+	InternalStatus             *string            `json:"internalStatus,omitempty"`
+	Order                      *int               `json:"order,omitempty"`
+	ShowUptime                 *bool              `json:"showUptime,omitempty"`
+	CreatedAt                  *time.Time         `json:"createdAt,omitempty"`
+	UpdatedAt                  *time.Time         `json:"updatedAt,omitempty"`
+	ArchivedAt                 *time.Time         `json:"archivedAt,omitempty"`
+	SiteID                     *string            `json:"siteId,omitempty"`
+	UniqueEmail                *string            `json:"uniqueEmail,omitempty"`
+	OldGroup                   *string            `json:"oldGroup,omitempty"`
+	GroupID                    *string            `json:"groupId,omitempty"`
+	IsParent                   *bool              `json:"isParent,omitempty"`
+	IsCollapsed                *bool              `json:"isCollapsed,omitempty"`
+	MonitorID                  *string            `json:"monitorId,omitempty"`
+	NameHTML                   *string            `json:"nameHtml,omitempty"`
+	NameHTMLTranslationID      *string            `json:"nameHtmlTranslationId,omitempty"`
+	DescriptionHTML            *string            `json:"descriptionHtml,omitempty"`
+	DescriptionHTMLTranslation *string            `json:"descriptionHtmlTranslationId,omitempty"`
+	IsThirdParty               *bool              `json:"isThirdParty,omitempty"`
+	ThirdPartyStatus           *string            `json:"thirdPartyStatus,omitempty"`
+	ThirdPartyComponentID      *string            `json:"thirdPartyComponentId,omitempty"`
+	ThirdPartyComponentService *string            `json:"thirdPartyComponentServiceId,omitempty"`
+	ImportedFromStatuspage     *bool              `json:"importedFromStatuspage,omitempty"`
+	StartDate                  *time.Time         `json:"startDate,omitempty"`
+	Group                      *string            `json:"group,omitempty"`
+	Translations               *ComponentLanguage `json:"translations,omitempty"`
+}
+
+type ComponentLanguage struct {
+	Name        *string `json:"name,omitempty"`
 	Description *string `json:"description,omitempty"`
-	Status      *string `json:"status,omitempty"`
-	Order       *int64  `json:"order,omitempty"`
-	ShowUptime  *bool   `json:"showUptime,omitempty"`
-	Grouped     *bool   `json:"grouped,omitempty"`
-	Group       *string `json:"group,omitempty"`
 }
 
 type Group struct {
 	Name *string `json:"name,omitempty"`
 }
 
-type ComponentFull struct {
-	Component
-	ID          *string `json:"id"`
-	UniqueEmail *string `json:"uniqueEmail,omitempty"`
-	GroupID     *string `json:"groupId,omitempty"`
-	Group       Group   `json:"group,omitempty"`
-}
-
-func (client *Client) CreateComponent(pageID string, component *Component) (*ComponentFull, error) {
-	var c ComponentFull
+func (client *Client) CreateComponent(pageID string, component *Component) (*Component, error) {
+	var c Component
 	err := createResource(
 		client,
 		pageID,
@@ -37,15 +60,15 @@ func (client *Client) CreateComponent(pageID string, component *Component) (*Com
 	return &c, err
 }
 
-func (client *Client) GetComponent(pageID string, componentID string) (*ComponentFull, error) {
-	var c ComponentFull
+func (client *Client) GetComponent(pageID string, componentID string) (*Component, error) {
+	var c Component
 	err := readResource(client, pageID, componentID, componentName, &c)
 
 	return &c, err
 }
 
-func (client *Client) UpdateComponent(pageID string, componentID string, component *Component) (*ComponentFull, error) {
-	var c ComponentFull
+func (client *Client) UpdateComponent(pageID string, componentID string, component *Component) (*Component, error) {
+	var c Component
 
 	err := updateResource(
 		client,
